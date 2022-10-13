f9d46c271725b8f87c119d6ea86420e05f166e6c
Alex <ortwinkrausemail@gmail.com>
2022-10-13 19:31:14 +0300

    update for switch case

--- 01_FirstProgram/002_switch_case.cpp
+++ 01_FirstProgram/002_switch_case.cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-
-	enum Directions
-	{
-		left = 'l',
-		right = 'r',
-		up = 'u',
-		down = 'd'
-	};
-
-	Directions direction{ left };
-	char temp{};
-	cout << "Enter a Direction " << endl;
-	cout << "l left" << endl;
-	cout << "r right" << endl;
-	cout << "u up" << endl;
-	cout << "d down" << endl;
-
-	cin >> temp;
-
-	switch (Directions(temp)) {
-	case left:
-		cout << "Your choise is left" << endl;
-		break;
-	case right:
-		cout << "Your choise is right" << endl;
-		break;
-	case up:
-		cout << "Your choise is up" << endl;
-		break;
-	case down:
-		cout << "Your choise is down" << endl;
-		break;
-	default:
-		cout << "Your choise is wrong" << endl;
-
-	}
-
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//	enum Directions
+//	{
+//		left = 'l',
+//		right = 'r',
+//		up = 'u',
+//		down = 'd'
+//	};
+//
+//	Directions direction{ left };
+//	char temp{};
+//	cout << "Enter a Direction " << endl;
+//	cout << "l left" << endl;
+//	cout << "r right" << endl;
+//	cout << "u up" << endl;
+//	cout << "d down" << endl;
+//
+//	cin >> temp;
+//
+//	switch (Directions(temp)) {
+//	case left:
+//		cout << "Your choise is left" << endl;
+//		break;
+//	case right:
+//		cout << "Your choise is right" << endl;
+//		break;
+//	case up:
+//		cout << "Your choise is up" << endl;
+//		break;
+//	case down:
+//		cout << "Your choise is down" << endl;
+//		break;
+//	default:
+//		cout << "Your choise is wrong" << endl;
+//
+//	}
+//
+//
+//	return 0;
+//}
