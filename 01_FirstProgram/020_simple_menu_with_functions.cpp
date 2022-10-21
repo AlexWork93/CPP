d349cde7d9df6c11df8d9c92d631084f2c4aa5bc
Alex <ortwinkrausemail@gmail.com>
2022-10-21 19:45:47 +0300

    work with functions, recursion and challenge

---
+++ 01_FirstProgram/020_simple_menu_with_functions.cpp
@@ -0,0 +1,11 @@
+// This challenge is about using a collection (list) of integers and allowing the user
+// to select option from a menu to perform operations on the list
+
+// Your program should display a menu options to the user as follows:
+
+// P - Print numbers
+// A - Add numbers
+// M - Display mean of the numbers
+// S - Display the smallest of the numbers
+// L - Display the largest of the numbers
+// Q - Quit
